9bd925624988e70ec4e34b91cbd1278d81ec06bc
zhangjicheng <zhangjichengcc@163.com>
2022-11-18 00:46:02 +0800

    update post javascript 作用域

--- testDemo/scope.js
+++ testDemo/scope.js
@@ -1,18 +1,43 @@
-function func1() {
-  const value1 = 'value1';
-  function func2() {
-    const value2 = 'value2';
-    function func3 () {
-      const value3 = 'value3';
-      console.log(value1);
-      console.log(value2);
-      console.log(value3);
-    }
-    return func3;
-  }
-  return func2;
-}
+/*
+ * @Author: zhangjicheng
+ * @Date: 2022-11-17 23:58:13
+ * @LastEditors: zhangjicheng
+ * @LastEditTime: 2022-11-18 00:45:40
+ * @FilePath: /docs/testDemo/scope.js
+ */
+// function func1() {
+//   const value1 = 'value1';
+//   function func2() {
+//     const value2 = 'value2';
+//     function func3 () {
+//       const value3 = 'value3';
+//       console.log(value1);
+//       console.log(value2);
+//       console.log(value3);
+//     }
+//     return func3;
+//   }
+//   return func2;
+// }
 
-const fn2 = func1();
-const fn3 = fn2();
-fn3();
+// const fn2 = func1();
+// const fn3 = fn2();
+// fn3();
+
+(function(){
+  function doSomething(value, fn){
+    fn(value);
+  }
+
+  function logName(name) {
+    console.log(name);
+    console.log(doSomething)
+    debugger
+  }
+
+  doSomething('tom', logName);
+  // doSomething('tom', function(name){
+  //   console.log(name);
+  //   debugger
+  // })
+})()
